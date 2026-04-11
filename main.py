3f2ac4f4b5101763b006db125c597177f55a094e
YoussefSNK <YoussefSNK@users.noreply.github.com>
2026-04-11 18:36:40 +0200

    ci : la structure en avance

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-from fastapi import FastAPI
+import uvicorn
 
-app = FastAPI()
-
-
-@app.get("/")
-def root():
-    return {"message": "ok"}
+if __name__ == "__main__":
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
