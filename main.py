8dadc95f41f8cd7d38201a99e31fe9583d1beb98
YoussefSNK <148006064+YoussefSNK@users.noreply.github.com>
2026-04-12 13:58:55 +0200

    chore : pouvoir lancer le projet avec python main.py

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {"message": "ok"}
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
